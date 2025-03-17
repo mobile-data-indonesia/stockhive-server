2e34e41a17491b0ac227f225945e5f81bf3f4cf5
michaeltio <tio.michael2004@gmail.com>
2025-03-17 20:52:22 +0700

    ini test jenkins ke 3

--- internal/config/db.go
+++ internal/config/db.go
@@ -15,7 +15,7 @@ var (
 	once sync.Once
 )
 
-//jenkins testtttttt banget ke 4
+//jenkins testtttttt banget ke 5
 func ConnectDB() *gorm.DB {
 	once.Do(func() {
 		dsn := "host=localhost user=postgres password=root dbname=stockhive port=5432 sslmode=disable TimeZone=Asia/Jakarta"
